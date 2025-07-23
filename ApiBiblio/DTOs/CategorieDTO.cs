10a0245eab7a286b849724cec26d41fb84f1c1ac
HazelCunuder <basile.cunuder@gmail.com>
2025-07-23 14:56:04 +0200

    feat: add CategorieDTO

---
+++ ApiBiblio/DTOs/CategorieDTO.cs
@@ -0,0 +1,16 @@
+﻿using ApiBiblio.Models;
+
+namespace ApiBiblio.DTOs
+{
+    public class CategorieDTO
+    {
+        public int Id { get; set; }
+
+        public required string NomCategorie { get; set; }
+
+        public CategorieDTO() { }
+
+        public CategorieDTO(Categorie categorie) =>
+            (Id, NomCategorie) = (categorie.Id, categorie.NomCategorie);
+    }
+}
