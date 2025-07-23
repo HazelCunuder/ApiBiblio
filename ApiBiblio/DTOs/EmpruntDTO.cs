d77a51a4f28acbf1ecb02cbab7f14eb3857ecfd4
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-07-23 14:54:59 +0200

    feat : add emprunt dto

---
+++ ApiBiblio/DTOs/EmpruntDTO.cs
@@ -0,0 +1,18 @@
+﻿using ApiBiblio.Models;
+
+namespace ApiBiblio.DTOs
+{
+    public class EmpruntDTO
+    {
+        public int Id { get; set; }
+        public DateOnly DateEmprunt { get; set; }
+        public bool Statut { get; set; }
+        public DateOnly DateRetour { get; set; }
+        public int MembreId { get; set; }
+
+        public EmpruntDTO() { }
+        public EmpruntDTO(Emprunt EmpruntItem) => 
+
+            (Id, DateEmprunt, DateRetour, MembreId) = (EmpruntItem.Id, EmpruntItem.DateRetour, EmpruntItem.DateEmprunt, EmpruntItem.MembreId)
+    }
+}
