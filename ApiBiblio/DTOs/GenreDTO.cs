a94dbd589f80ea9db6b9f9ae13b09b90763d1468
HazelCunuder <basile.cunuder@gmail.com>
2025-07-23 14:53:00 +0200

    feat: add GenreDTO

---
+++ ApiBiblio/DTOs/GenreDTO.cs
@@ -0,0 +1,15 @@
+﻿using ApiBiblio.Models;
+
+namespace ApiBiblio.DTOs
+{
+    public class GenreDTO
+    {
+        public int Id { get; set; }
+        public required string NomGenre { get; set; }
+
+        public GenreDTO() { }
+
+        public GenreDTO(Genre genre) =>
+            (Id, NomGenre) = (genre.Id, genre.NomGenre);
+    }
+}
