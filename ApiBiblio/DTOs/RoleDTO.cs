00c651839d452f36156de187fc7f82084ba00254
abdellah59 <abdellah.ait59@gmail.com>
2025-07-23 15:18:25 +0200

    feat: create RoleDTO class with properties and constructors

---
+++ ApiBiblio/DTOs/RoleDTO.cs
@@ -0,0 +1,13 @@
+﻿using ApiBiblio.Models;
+
+namespace ApiBiblio.DTOs
+{
+    public class RoleDTO
+    {
+        public int Id { get; set; }
+        public required string NomRole { get; set; }
+        public RoleDTO() { }
+        public RoleDTO(Role role) =>
+            (Id, NomRole) = (role.Id, role.NomRole);
+    }
+}
