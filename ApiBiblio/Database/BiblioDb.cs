b9802a0b5f82e8c23704f9c75c6650462c5802d7
HazelCunuder <basile.cunuder@gmail.com>
2025-07-23 10:54:51 +0200

    feat: add tables to db

--- ApiBiblio/Database/BiblioDb.cs
+++ ApiBiblio/Database/BiblioDb.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ApiBiblio.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiBiblio.Database
 {
     public class BiblioDb : DbContext
     {
+        public BiblioDb(DbContextOptions<BiblioDb> options) : base(options) { }
 
+        public DbSet<Employe> Employes => Set<Employe>();
+        public DbSet<Role> Roles => Set<Role>();
+        public DbSet<Membre> Membres => Set<Membre>();
+        public DbSet<Livre> Livres => Set<Livre>();
+        public DbSet<Auteur> Auteurs => Set<Auteur>();
+        public DbSet<Categorie> Categories => Set<Categorie>();
+        public DbSet<Genre> Genres => Set<Genre>();
+        public DbSet<Emprunt> Emprunts => Set<Emprunt>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+
+        }
     }
 }
