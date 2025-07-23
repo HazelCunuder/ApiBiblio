9e10ca7db47d4fd13629c62c048fd494f2224dab
HazelCunuder <basile.cunuder@gmail.com>
2025-07-23 14:40:21 +0200

    feat: add LivreDTO

---
+++ ApiBiblio/DTOs/LivreDTO.cs
@@ -0,0 +1,22 @@
+﻿using ApiBiblio.Models;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiBiblio.DTOs
+{
+    public class LivreDTO
+    {
+        public int Id { get; set; }
+
+        public required string Titre { get; set; }
+        public bool Disponible { get; set; }
+        public int AnneePublication { get; set; }
+        public string? ISBN { get; set; }
+        public int IdEmprunt { get; set; }
+        public int IdCategorie { get; set; }
+
+        public LivreDTO() { }
+        public LivreDTO(Livre Livre) =>
+            (Id, Titre, Disponible, AnneePublication, ISBN, IdEmprunt, IdCategorie) = (Livre.Id, Livre.Titre, Livre.Disponible, Livre.AnnePublication, Livre.ISBN, Livre.IdEmprunt, Livre.IdCategorie);
+    }
+}
