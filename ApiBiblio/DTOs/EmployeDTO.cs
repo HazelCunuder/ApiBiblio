a482a35e0f965eeb3d6563dfae2a59e97dd39bf1
abdellah59 <abdellah.ait59@gmail.com>
2025-07-23 15:18:05 +0200

    feat: add properties to EmployeDTO

--- ApiBiblio/DTOs/EmployeDTO.cs
+++ ApiBiblio/DTOs/EmployeDTO.cs
@@ -2,5 +2,9 @@
 {
     public class EmployeDTO
     {
+        public int Id { get; set; }
+        public required string Nom { get; set; }
+        public required string Email { get; set; }
+        public required string Role { get; set; }
     }
 }
