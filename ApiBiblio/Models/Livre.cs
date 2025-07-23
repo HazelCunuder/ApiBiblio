ba43ad720bbdf7f93090937788f03074039a4374
HazelCunuder <basile.cunuder@gmail.com>
2025-07-23 10:01:59 +0200

    feat: add parameters for the Livre table

---
+++ ApiBiblio/Models/Livre.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiBiblio.Models
+{
+    public class Livre
+    {
+        public int Id { get; set; }
+        public required string Titre { get; set; }
+        public bool Disponible { get; set; } = true;
+        public int AnnePublication { get; set; }
+        public string? ISBN { get; set; }
+        public int CategorieId { get; set; }
+        public int EmpruntId { get; set; }
+    }
+}
