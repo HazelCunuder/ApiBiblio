c9c525a7b037c223a3983c82efc26c3ab6aae32c
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-07-23 09:57:09 +0200

    feat: add the membre model

---
+++ ApiBiblio/Models/Membre.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiBiblio.Models
+{
+    public class Membre
+    {
+        public int Id { get; set; }
+        public string? nom_Membre{ get; set; }
+        public string? prenom_Membre { get; set; }
+        public string? mdp_Membre { get; set; }
+        public string? adresse_postale {  get; set; }
+        public string? adresse_mail { get; set; }
+        public string? telephone {  get; set; }
+        public string? historique_Membre { get; set; }
+    }
+}
