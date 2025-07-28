31e9c527685286ce76fea382698b9a532dc99b74
HazelCunuder <basile.cunuder@gmail.com>
2025-07-28 15:41:10 +0200

    feat: add data validation for Emprunt

--- ApiBiblio/Models/Emprunt.cs
+++ ApiBiblio/Models/Emprunt.cs
@@ -1,11 +1,23 @@
-﻿namespace ApiBiblio.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiBiblio.Models
 {
     public class Emprunt
     {
         public int Id { get; set; }
+
+        [Display(Name = "Date de l'emprunt")]
+        [DataType(DataType.Date)]
         public DateOnly DateEmprunt { get; set; }
-        public required bool Statut {  get; set; }
+
+        [Display(Name = "Rendu ?")]
+        public required bool Statut { get; set; }
+
+        [Display(Name = "Date de retour")]
+        [DataType(DataType.Date)]
         public DateOnly DateRetour { get; set; }
+
         public int IdMembre { get; set; }
     }
 }
