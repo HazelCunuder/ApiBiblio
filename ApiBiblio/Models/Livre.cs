f0ce1ead306f16562c4e44293991bcf88abc6355
HazelCunuder <basile.cunuder@gmail.com>
2025-07-28 14:45:54 +0200

    feat: add verification for Livre

--- ApiBiblio/Models/Livre.cs
+++ ApiBiblio/Models/Livre.cs
@@ -1,11 +1,18 @@
-﻿namespace ApiBiblio.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiBiblio.Models
 {
     public class Livre
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public required string Titre { get; set; }
         public bool Disponible { get; set; } = true;
-        public int AnnePublication { get; set; }
+        public Int16 AnnePublication { get; set; }
+
+        [StringLength(17, MinimumLength = 13)]
         public string? ISBN { get; set; }
         public int IdCategorie { get; set; }
     }
