6e8e681606ea6292fbdf9568ca3873d970fa8932
HazelCunuder <basile.cunuder@gmail.com>
2025-07-28 15:35:39 +0200

    feat: add data validation for Auteur

--- ApiBiblio/Models/Auteur.cs
+++ ApiBiblio/Models/Auteur.cs
@@ -1,9 +1,17 @@
-﻿namespace ApiBiblio.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiBiblio.Models
 {
     public class Auteur
     {
         public int Id { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(200)]
         public string? NomAuteur { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [StringLength(200)]
         public string? PrenomAuteur { get; set; }
     }
 }
