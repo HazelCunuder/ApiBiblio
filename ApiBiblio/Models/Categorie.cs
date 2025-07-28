e0df90d3d0824a85a686f0b3fb042709d9ee9ddc
HazelCunuder <basile.cunuder@gmail.com>
2025-07-28 14:58:06 +0200

    feat: add data validation for Categorie

--- ApiBiblio/Models/Categorie.cs
+++ ApiBiblio/Models/Categorie.cs
@@ -1,8 +1,14 @@
-﻿namespace ApiBiblio.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiBiblio.Models
 {
     public class Categorie
     {
         public int Id { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public required string NomCategorie { get; set; }
     }
 }
