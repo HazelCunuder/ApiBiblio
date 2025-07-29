55bd9d028cf14f61f38438244955537a0412ac23
HazelCunuder <basile.cunuder@gmail.com>
2025-07-29 09:24:14 +0200

    feat: add Error Message for Livre

--- ApiBiblio/Models/Livre.cs
+++ ApiBiblio/Models/Livre.cs
@@ -7,13 +7,13 @@ namespace ApiBiblio.Models
         public int Id { get; set; }
 
         [StringLength(60, MinimumLength = 3)]
-        [Required]
+        [Required(ErrorMessage = "Veuillez insérez un titre")]
         public required string Titre { get; set; }
         public bool Disponible { get; set; } = true;
         public Int16 AnnePublication { get; set; }
 
         [StringLength(17, MinimumLength = 13)]
-        [RegularExpression(@"^(?:ISBN(?:-13)?:?\ )?(?=[0-9]{13}$|(?=(?:[0-9]+[-\ ]){4})[-\ 0-9]{17}$)97[89][-\ ]?[0-9]{1,5}[-\ ]?[0-9]+[-\ ]?[0-9]+[-\ ]?[0-9]$")]
+        [RegularExpression(@"^(?:ISBN(?:-13)?:?\ )?(?=[0-9]{13}$|(?=(?:[0-9]+[-\ ]){4})[-\ 0-9]{17}$)97[89][-\ ]?[0-9]{1,5}[-\ ]?[0-9]+[-\ ]?[0-9]+[-\ ]?[0-9]$", ErrorMessage = "Veuillez entrer un ISBN valide")]
         public string? ISBN { get; set; }
         public int IdCategorie { get; set; }
     }
