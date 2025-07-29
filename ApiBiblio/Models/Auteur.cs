15f152f60c08f2733adec34693d214cb5e593ad7
HazelCunuder <basile.cunuder@gmail.com>
2025-07-29 09:29:50 +0200

    feat: add Error Messages

--- ApiBiblio/Models/Auteur.cs
+++ ApiBiblio/Models/Auteur.cs
@@ -6,13 +6,13 @@ namespace ApiBiblio.Models
     {
         public int Id { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[A-zà-ÿ\s'-]+$")]
-        [Required]
+        [RegularExpression(@"^[A-Z]+[A-zà-ÿ\s'-]+$", ErrorMessage = "Veuillez entrer un nom valide")]
+        [Required(ErrorMessage = "Vous ne pouvez pas laisser ce champ vide")]
         [StringLength(150)]
         public string? NomAuteur { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[A-zà-ÿ\s'-]+$")]
-        [Required]
+        [RegularExpression(@"^[A-Z]+[A-zà-ÿ\s'-]+$", ErrorMessage = "Veuillez entrer un prénom valide")]
+        [Required(ErrorMessage = "Vous ne pouvez pas laisser ce champ vide")]
         [StringLength(150)]
         public string? PrenomAuteur { get; set; }
     }
