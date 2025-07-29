1c94bc6e2391e6a9d9b8d25e8bb6287a7924a9d0
HazelCunuder <basile.cunuder@gmail.com>
2025-07-29 09:27:26 +0200

    feat: add Error Message for Employe

--- ApiBiblio/Models/Employe.cs
+++ ApiBiblio/Models/Employe.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
+using System.Runtime.InteropServices;
 
 namespace ApiBiblio.Models
 {
@@ -7,22 +9,22 @@ namespace ApiBiblio.Models
     {
         public int Id { get; set; }
 
-        [RegularExpression(@"[A-Z]+[A-zà-ÿ\s'-]+$")]
+        [RegularExpression(@"[A-Z]+[A-zà-ÿ\s'-]+$", ErrorMessage = "Veuillez entrer un nom valide")]
         [StringLength(200)]
-        [Required]
+        [Required(ErrorMessage = "Vous ne pouvez pas laisser ce champ vide")]
         public required string NomEmploye { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[A-zà-ÿ\s'-]+$")]
+        [RegularExpression(@"^[A-Z]+[A-zà-ÿ\s'-]+$", ErrorMessage = "Veuillez entrer un prénom valide")]
         [StringLength(200)]
-        [Required]
+        [Required(ErrorMessage = "Vous ne pouvez pas laisser ce champ vide")]
         public required string PrenomEmploye { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Vous devez rentrer un nom d'utilisateur")]
         public required string LoginEmploye { get; set; } // Identifiant utilisé pour se connecter
 
-        [Required]
-        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$")]
-        [StringLength(16, MinimumLength = 8)]
+        [Required(ErrorMessage = " Vous ne pouvez pas laisser ce champ vide")]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$", ErrorMessage = "Le mot de passe doit contenir un chiffre, une lettre en Majuscule, une lettre en mininuscule et un caractère spécial")]
+        [StringLength(16, ErrorMessage = "Le mot de passe doit contenir entre 8 et 16 caractères.", MinimumLength = 8)]
         public required string MdpEmploye { get; set; } 
     }
 }
