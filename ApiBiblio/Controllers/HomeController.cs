fef057ae51f6c989aa9276c5f543d7c4204d4343
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-07-31 11:07:47 +0200

    fix : fix home for the dashboard

--- ApiBiblio/Controllers/HomeController.cs
+++ ApiBiblio/Controllers/HomeController.cs
@@ -31,21 +31,34 @@ namespace ApiBiblio.Controllers
         [Authorize]
         public async Task<IActionResult> Dashboard()
         {
-            var stats = new DashboardStats
-            {
-                LivreCount = await _context.Livres.CountAsync(),
-                LivreDispoCount = await _context.Livres.CountAsync(l => l.Disponible),
-                EmpruntEnCoursCount = await _context.Emprunts.CountAsync(e => e.DateRetour == null),
-                MembreCount = await _context.Membres.CountAsync(),
-                EmpruntsRecents = await _context.Emprunts
-                    .OrderByDescending(e => e.DateEmprunt)
-                    .Include(e => e.Membre)
-                    .Include(e => e.Livre)
-                    .Take(5)
-                    .ToListAsync()
-            };
+            // Statistiques simples
+            var totalLivres = await _context.Livres.CountAsync();
+            var livresDisponibles = await _context.Livres.CountAsync(l => l.Disponible);
+            var empruntsEnCours = await _context.Emprunts.CountAsync(e => e.DateRetour == null);
+            var membresActifs = await _context.Membres.CountAsync();
 
-            return View(stats);
+            // Emprunts récents (on prend les 5 derniers)
+            var empruntsRecents = await _context.Emprunts
+                .Include(e => e.Membre)
+                .Include(e => e.Livre)
+                .OrderByDescending(e => e.DateEmprunt)
+                .Take(5)
+                .Select(e => new
+                {
+                    Date = e.DateEmprunt,
+                    Membre = e.Membre != null ? e.Membre.PrenomMembre + " " + e.Membre.NomMembre : "",
+                    Livre = e.Livre != null ? e.Livre.Titre : "",
+                    Statut = e.DateRetour == null ? "En cours" : "Terminé"
+                })
+                .ToListAsync();
+
+            ViewBag.TotalLivres = totalLivres;
+            ViewBag.LivresDisponibles = livresDisponibles;
+            ViewBag.EmpruntsEnCours = empruntsEnCours;
+            ViewBag.MembresActifs = membresActifs;
+            ViewBag.EmpruntsRecents = empruntsRecents;
+
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
