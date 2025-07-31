0d9f0945e7533743fe8e06adabdaff3c9b6a8955
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2025-07-31 10:03:26 +0200

    feat : add dashboard in home controller

--- ApiBiblio/Controllers/HomeController.cs
+++ ApiBiblio/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
+using ApiBiblio.Database;
 using ApiBiblio.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ApiBiblio.Controllers
@@ -8,10 +10,12 @@ namespace ApiBiblio.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BiblioDb _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BiblioDb context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -25,9 +29,23 @@ namespace ApiBiblio.Controllers
         }
 
         [Authorize]
-        public IActionResult Dashboard()
+        public async Task<IActionResult> Dashboard()
         {
-            return View();
+            var stats = new DashboardStats
+            {
+                LivreCount = await _context.Livres.CountAsync(),
+                LivreDispoCount = await _context.Livres.CountAsync(l => l.Disponible),
+                EmpruntEnCoursCount = await _context.Emprunts.CountAsync(e => e.DateRetour == null),
+                MembreCount = await _context.Membres.CountAsync(),
+                EmpruntsRecents = await _context.Emprunts
+                    .OrderByDescending(e => e.DateEmprunt)
+                    .Include(e => e.Membre)
+                    .Include(e => e.Livre)
+                    .Take(5)
+                    .ToListAsync()
+            };
+
+            return View(stats);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
